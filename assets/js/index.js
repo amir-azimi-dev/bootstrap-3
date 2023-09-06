8455fe24bb720c95f072e90cc2eeebe6e9ae6a28
Amir Azimi <azymy3354@gmail.com>
2023-09-06 13:48:01 +0330

    add: header

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// new PureCounter({
+//     selector: ".purecounter",
+//     start: 0,
+//     end: 100,
+//     duration: 2,
+//     delay: 10,
+//     once: true,
+//     pulse: false,
+//     decimals: 0,
+//     legacy: true,
+//     filesizing: false,
+//     currency: false,
+//     formater: "us-US",
+//     separator: false,
+// });
+
+new PureCounter();
